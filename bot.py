6317e9eb893bea9b564f89d32b4b780b54bfa00c
nprimo <primo.niccolo@gmail.com>
2023-01-31 20:37:51 +0100

    feat and refactor:
    - add decorator to check av len for bot commands
    - move utils functions in a separate file

--- bot.py
+++ bot.py
@@ -1,70 +1,60 @@
 import discord
-import urllib
-from db import add_new_car, get_car_raw_info, get_cars_available,\
-    is_car_in_db, get_car_last_info, update_car
+import db
+from utils import check_av_len, is_location_valid, car_not_in_db
 
 
 def hello(message: discord.Message, av: list[str]) -> str:
     return f'Hello {message.author.mention}'
 
 
+@check_av_len([2])
 def add(message: discord.Message, av: list[str]) -> str:
-    if len(av) != 2:
-        return 'Wrong number of arguments! Run: $add <name of the car>'
     car_name = av[1].lower()
-    if is_car_in_db(car_name):
-        return f'{car_name} is already in the database!'
+    if db.is_car_in_db(car_name):
+        return db.get_car_last_info(car_name)
     # maybe check if the user is allowed to add
-    return add_new_car(av[1])
+    return db.add_new_car(av[1])
 
 
+@check_av_len([1, 2])
 def where(message: discord.Message, av: list[str]) -> str:
-    if len(av) != 2:
-        return 'Wrong number of arguments! Run: $where <name of the car>'
+    if len(av) == 1:
+        cars_available = db.get_cars_available()
+        return '\n'.join([
+            f'{car}: {db.get_car_last_info(car)}'
+            for car in cars_available
+        ])
     car_name = av[1].lower()
-    # add "all" option that showcases the postion of all the cars
-    if is_car_in_db(car_name):
-        return get_car_last_info(car_name)
-    return f'{car_name} is not in the database! Add the car with $add command'
+    if db.is_car_in_db(car_name):
+        return db.get_car_last_info(car_name)
+    return car_not_in_db(car_name)
 
 
-def is_location_valid(new_location: str) -> bool:
-    parsed_url = urllib.parse.urlparse(new_location)
-    if 'goo.gl' in parsed_url.netloc and\
-            ('maps' in parsed_url.netloc or 'maps' in parsed_url.path):
-        return True
-    return False
-
-
+@check_av_len([3])
 def update(message: discord.Message, av: list[str]) -> str:
-    if len(av) != 3:
-        return """Wrong number of arguments!
-    Run: $update <name of the car> <new poistion - google maps link>"""
     car_name = av[1].lower()
-    if is_car_in_db(car_name) and is_location_valid(av[2]):
+    if db.is_car_in_db(car_name) and is_location_valid(av[2]):
         print(f'location valid: {is_location_valid(av[2])}')
         # maybe check if the user is allowed to update position
         new_info = {
             'location': av[2],
             'user': str(message.author)
         }
-        return update_car(car_name, new_info)
+        return db.update_car(car_name, new_info)
     return f'{car_name} is not in the database! Add the car with $add command'
 
 
+@check_av_len([1, 2])
 def info(message: discord.Message, av: list[str]) -> str:
-    if len(av) != 2:
-        return """Wrong number of arguments!
-    Run: $info <name of car> or $info all
-    """
-    car_name = av[1].lower()
-    if is_car_in_db(car_name):
-        msg = get_car_raw_info(car_name)
-    elif car_name == 'all':
+    if len(av) == 1:
         msg = 'The available cars are:\n'
-        msg += '\n'.join(get_cars_available())
+        msg += '\n'.join(db.get_cars_available())
+        return msg
+    car_name = av[1].lower()
+    if db.is_car_in_db(car_name):
+        msg = db.get_car_raw_info(car_name)
     else:
-        msg = f'{car_name} not available'
+        msg = car_not_in_db(car_name)
     return msg
 
 
---
+++ utils.py
@@ -0,0 +1,28 @@
+import urllib
+import discord
+
+# Error messages
+
+
+def car_not_in_db(car_name: str) -> str:
+    return f'{car_name} is not in the database!'
+
+
+# functions
+def is_location_valid(new_location: str) -> bool:
+    parsed_url = urllib.parse.urlparse(new_location)
+    if 'goo.gl' in parsed_url.netloc and\
+            ('maps' in parsed_url.netloc or 'maps' in parsed_url.path):
+        return True
+    return False
+
+
+def check_av_len(len_accepted: list[int]):
+    def inner(func):
+        def wrapped(message: discord.Message, av: list[str]):
+            if len(av) in len_accepted:
+                return func(message, av)
+            else:
+                return f'Error! Run command with number of args in {len_accepted}'
+        return wrapped
+    return inner
